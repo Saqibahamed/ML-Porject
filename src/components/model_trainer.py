58c1b03566fb61de5158f8c4b06e0b65147add17
Sharief <saqib.ahamed.sharief.sharief@hp.com>
2026-05-05 16:41:03 +0530

    Hyperparameter Tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -43,13 +43,55 @@ class ModelTrainer:
                         'KNN' : KNeighborsRegressor(),
                         'Decision Tree' : DecisionTreeRegressor(),
                         'Random Forest' : RandomForestRegressor(),
-                        'Ada Boost' : AdaBoostRegressor(),
-                        'Gradient Boost' : GradientBoostingRegressor(),
-                        'Cat Boost' : CatBoostRegressor(),
-                        'XGB' : XGBRegressor()
+                        'AdaBoost Regressor' : AdaBoostRegressor(),
+                        'Gradient Boosting' : GradientBoostingRegressor(),
+                        'CatBoosting Regressor' : CatBoostRegressor(),
+                        'XGBRegressor' : XGBRegressor()
                     }
+            
+            params={
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                },
+                "Random Forest":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
                  
-            model_report:dict=evaluate_model(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models)
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Linear Regression":{},
+                "Ridge": {},
+                "Lasso": {},
+                "SVR": {},
+                "KNN": {}, 
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoosting Regressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                }
+                
+            }
+                 
+            model_report:dict=evaluate_model(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,models=models,param=params)
 
             ## to get best model score from dict
             best_model_score = max(sorted(model_report.values()))
@@ -71,6 +113,8 @@ class ModelTrainer:
                 file_path=self.model_trainer_config.trained_model_data_path,
                 obj= best_model
                 )
+            
+            logging.info("Saved model.pkl file")
 
             predicted = best_model.predict(X_test)
             r2 = r2_score(y_test,predicted)
--- src/utils.py
+++ src/utils.py
@@ -2,8 +2,12 @@ import sys
 import os
 import dill
 
+
+from src import logger 
+import logging
 from src.exception import CustomException
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 
 def save_file(file_path,obj):
@@ -19,13 +23,24 @@ def save_file(file_path,obj):
         raise CustomException(e,sys)
     
 
-def evaluate_model(X_train,y_train,X_test,y_test,models):
+def evaluate_model(X_train,y_train,X_test,y_test,models,param):
     try:
         report={}
 
         for name,model in models.items():
-            
-            model.fit(X_train,y_train) # train model
+
+            para=param[name]
+
+            gs = GridSearchCV(model,para,cv=3)
+
+            gs.fit(X_train,y_train)
+
+            logging.info(f"{name} best params: {gs.best_params_}")
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
+            #model.fit(X_train,y_train) # train model
 
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
